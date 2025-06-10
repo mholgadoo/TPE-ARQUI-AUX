24eb61acd97d0e4bde3341dc048bfeacfee3f7c1
matias <mholgado@itba.edu.ar>
2025-06-10 00:22:35 -0300

    cambios en pongis

--- x64BareBones/run.sh
+++ x64BareBones/run.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+qemu-system-x86_64 \
+  -hda Image/x64BareBonesImage.qcow2 \
+  -m 512 \
+  -machine pcspk-audiodev=snd0 \
+  -audiodev pa,id=snd0
